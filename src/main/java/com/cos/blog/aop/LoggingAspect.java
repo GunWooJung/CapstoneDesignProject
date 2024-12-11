be585e76f1655a2983369f807646152744881cc6
unknown <j3415879@naver.com>
2024-12-11 18:27:09 +0900

    aop

--- src/main/java/com/cos/blog/aop/LoggingAspect.java
+++ src/main/java/com/cos/blog/aop/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.cos.blog.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -18,7 +19,7 @@ public class LoggingAspect {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-	// 시큐리티 로그인 메서드 실행 전후에 적용s
+	// 시큐리티 로그인 메서드 실행 전후에 적용ss
 	@Around("execution(* com.cos.blog.api.PlaceApiController.getPlaces(..))")
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 		long start = System.currentTimeMillis();
@@ -33,16 +34,26 @@ public class LoggingAspect {
 			// 메서드명 및 실행시간 로그
 			MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 			String methodName = signature.getDeclaringTypeName() + "." + signature.getName();
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-			if (authentication != null && authentication.isAuthenticated()) {
-				// 인증된 사용자의 정보
-				PrincipalDetail user = (PrincipalDetail) authentication.getPrincipal();
-				String username = user.getUsername();
-				logger.info("현재 로그인한 사용자: " + username + " , 실행 api : " + methodName + ", 실행 시간 : " + executionTime + "ms");
-			}
+			logger.info("실행 api : " + methodName + ", 실행 시간 : " + executionTime + "ms");
 		}
 		return result;
+	}
+
+	@After("execution(* com.cos.blog.api.PlaceApiController.getPlaces(..))")
+	public void username() {
+		  // 현재 로그인한 사용자 정보를 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            // getPrincipal()이 반환하는 객체가 PrincipalDetail인지 확인 후 캐스팅
+            if (authentication.getPrincipal() instanceof PrincipalDetail) {
+                PrincipalDetail user = (PrincipalDetail) authentication.getPrincipal();
+                String username = user.getUsername();
+                logger.info("현재 로그인한 사용자: " + username);
+            } else {
+                // 만약 Principal이 PrincipalDetail이 아니면 String 등 다른 타입이므로 그에 맞게 처리
+                logger.info("로그인된 사용자 정보가 없습니다.");
+            }
+        }
 	}
 
 }
