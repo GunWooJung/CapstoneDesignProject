9de8f473e84e09962d111f9f47854e8cd9b29ab6
unknown <j3415879@naver.com>
2024-12-16 19:03:17 +0900

    https

--- src/main/java/com/cos/blog/config/WebConfig.java
+++ src/main/java/com/cos/blog/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		// 특정 URL 패턴에 대한 CORS 설정
 		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-				.allowedOrigins("http://localhost:8080") // 허용할 출처 (예: React 개발 서버)
+				.allowedOrigins("http://localhost:8080", "https://cap.30ticket.store") // 허용할 출처 (예: React 개발 서버)
 				.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
 				.allowedHeaders("*") // 허용할 헤더
 				.allowCredentials(true); // 자격 증명 포함 허용
