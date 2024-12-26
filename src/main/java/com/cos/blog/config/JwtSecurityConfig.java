32403cfea2f1551223d6d03c773362ea4fcc23f9
unknown <j3415879@naver.com>
2024-12-26 19:52:15 +0900

    jwt 활성화

--- src/main/java/com/cos/blog/config/JwtSecurityConfig.java
+++ src/main/java/com/cos/blog/config/JwtSecurityConfig.java
@@ -72,7 +72,7 @@ public class JwtSecurityConfig {
         }));
 
         // 4. JWT 필터 추가
-        //http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         //테스트 중 주석
         return http.build();
     }
